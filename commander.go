c0fd8ea5b798c9305e5eb2d416a2868edc9e264c
yukpiz <yukpiz@gmail.com>
2016-11-22 20:49:52 +0900

    Initialize.

---
+++ commander.go
@@ -0,0 +1,109 @@
+package commander
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+)
+
+type Connection struct {
+	Host     string
+	Port     int
+	User     string
+	Passwd   string
+	Protocol string
+	Client   *ssh.Client
+	Session  *ssh.Session
+}
+
+type PasswdConnection struct {
+	Connection Connection
+}
+
+type KeyConnection struct {
+	Connection Connection
+	PemPath    string
+	Signer     ssh.Signer
+}
+
+func (c *KeyConnection) Connect() ([]string, error) {
+	var logs []string
+	//Parsing SSH Private Key.
+	signer, err := getPrivateKey(c.PemPath)
+	if err != nil {
+		logs = append(logs, "Loading SSH Private Key File\t[ NG ]")
+		return logs, err
+	}
+	logs = append(logs, "Loading SSH Private Key File\t[ OK ]")
+	c.Signer = signer
+
+	//Generate SSH Configuration.
+	config := &ssh.ClientConfig{
+		User: c.Connection.User,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(c.Signer),
+		},
+	}
+	logs = append(logs, "Generate SSH Configuration\t[ OK ]")
+
+	//Connection.
+	client, err := connect(config, c.Connection.Host,
+		c.Connection.Port, c.Connection.Protocol)
+	if err != nil {
+		logs = append(logs, "SSH Connecting...\t[ NG ]")
+		return logs, err
+	}
+	logs = append(logs, "SSH Connecting...\t[ OK ]")
+	c.Connection.Client = client
+
+	//Generate New Session.
+	session, err := c.Connection.Client.NewSession()
+	if err != nil {
+		logs = append(logs, "Generate New Session\t[ NG ]")
+		return logs, err
+	}
+	logs = append(logs, "Generate New Session\t[ OK ]")
+	c.Connection.Session = session
+
+	return logs, nil
+}
+
+func (c *KeyConnection) Command(command string) ([]string, string) {
+	var logs []string
+	var b bytes.Buffer
+	logs = append(logs, fmt.Sprintf("Executing Command: %s", command))
+	c.Connection.Session.Stdout = &b
+	err := c.Connection.Session.Run(command)
+	if err != nil {
+		logs = append(logs, "Failed to executing command.")
+	} else {
+		logs = append(logs, "Complated Command!")
+	}
+	return logs, b.String()
+}
+
+func (c *KeyConnection) Dispose() error {
+	if err := c.Connection.Session.Close(); err != nil {
+		return err
+	}
+	if err := c.Connection.Client.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getPrivateKey(pemPath string) (ssh.Signer, error) {
+	buf, err := ioutil.ReadFile(pemPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.ParsePrivateKey(buf)
+}
+
+func connect(config *ssh.ClientConfig, host string,
+	port int, protocol string) (*ssh.Client, error) {
+	return ssh.Dial(protocol,
+		fmt.Sprintf("%s:%d", host, port), config)
+}
